981d7ed37ad6377c117d8f2419c64eaad9c982b4
Anthony Chao <anthonyqchao@gmail.com>
2018-11-16 16:28:44 -0800

    redid time windows

--- main.r
+++ main.r
@@ -15,37 +15,23 @@ train <- train[rowSums(is.na(train)) == 0,]
 
 # Create specific date intervals
 christmas <- train[month(train$Date) == 12 & day(train$Date) == 25,]
-thanksgiving <- subset(train, train$Date %in% date(c("2007-11-22", "2008-11-27", "2009-11-26")))
-black_friday <- subset(train, train$Date %in% date(c("2007-11-23", "2008-11-28", "2009-11-27")))
-
-weekends <- train[train$Day_of_week %in% c("Saturday", "Sunday"),]
-weekdays <- train[train$Day_of_week %in% c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"),]
+halloween <- train[month(train$Date) == 10 & day(train$Date) == 31,]
 
 winter <- subset(train, month(train$Date) %in% c(12, 1, 2))
 spring <- subset(train, month(train$Date) %in% c(3, 4, 5))
 summer <- subset(train, month(train$Date) %in% c(6, 7, 8))
 fall <- subset(train, month(train$Date) %in% c(9, 10, 11))
 
-#Exclude Christmas, Thanksgivings and Black Friday
-winter <- subset(winter, ! winter$Date %in% date(c("2007-11-22", "2008-11-27", "2009-11-26",
-                                           "2007-11-23", "2008-11-28", "2009-11-27",
-                                            "2007-12-25", "2008-12-25", "2009-12-25")))
-
 # Create time windows
-weekdays.evenings <- weekdays[hour(hms(weekdays$Time)) >= 18 & hour(hms(weekdays$Time)) < 24,]
-weekdays.nights <- weekdays[hour(hms(weekdays$Time)) >= 0 & hour(hms(weekdays$Time)) < 6, ]
-weekdays.mornings <- weekdays[hour(hms(weekdays$Time)) >= 6 & hour(hms(weekdays$Time)) < 12,]
-weekdays.afternoons <- weekdays[hour(hms(weekdays$Time)) >= 12 & hour(hms(weekdays$Time)) < 18,]
+summer.weekdays <- subset(summer, summer$Day_of_week %in% c("Monday, Tuesday", "Wednesday", "Thursday", "Friday"))
+summer.weekends <- subset(summer, summer$Day_of_week %in% c("Saturday", "Sunday"))
 
-weekends.evenings <- weekends[hour(hms(weekends$Time)) >= 18 & hour(hms(weekends$Time)) < 24,]
-weekends.nights <- weekends[hour(hms(weekends$Time)) >= 0 & hour(hms(weekends$Time)) < 6, ]
-weekends.mornings <- weekends[hour(hms(weekdays$Time)) >= 6 & hour(hms(weekends$Time)) < 12,]
-weekends.afternoons <- weekends[hour(hms(weekends$Time)) >= 12 & hour(hms(weekends$Time)) < 18,]
 
-summer.evenings <- summer[hour(hms(summer$Time)) >= 18 & hour(hms(summer$Time)) < 24,]
-summer.nights <- summer[hour(hms(summer$Time)) >= 0 & hour(hms(summer$Time)) < 6,]
-summer.mornings <- summer[hour(hms(summer$Time)) >= 6 & hour(hms(summer$Time)) < 12,]
-summer.afternoons <- summer[hour(hms(summer$Time)) >= 12 & hour(hms(summer$Time)) < 18,]
+
+summer.weekdays.evenings <- summer.weekdays[hour(hms(summer.weekdays$Time)) >= 18 & hour(hms(summer.weekdays$Time)) < 24,]
+summer.weekdays.nights <- summer.weekdays[hour(hms(summer.weekdays$Time)) >= 0 & hour(hms(summer.weekdays$Time)) < 6,]
+summer.weekdays.mornings <- summer.weekdays[hour(hms(summer$Time)) >= 6 & hour(hms(summer$Time)) < 12,]
+summer.weekdays.afternoons <- summer.weekdays[hour(hms(summer.weekdays$Time)) >= 12 & hour(hms(summer.weekdays$Time)) < 18,]
 
 fall.evenings <- fall[hour(hms(fall$Time)) >= 18 & hour(hms(fall$Time)) < 24,]
 fall.nights <- fall[hour(hms(fall$Time)) >= 0 & hour(hms(fall$Time)) < 6,]
@@ -66,16 +52,6 @@ christmas.evenings <- christmas[hour(hms(christmas$Time)) >= 18 & hour(hms(chris
 christmas.nights <- christmas[hour(hms(christmas$Time)) >= 0 & hour(hms(christmas$Time)) < 6,]
 christmas.mornings <- christmas[hour(hms(christmas$Time)) >= 6 & hour(hms(christmas$Time)) < 12,]
 christmas.afternoons <- christmas[hour(hms(christmas$Time)) >= 12 & hour(hms(christmas$Time)) < 18,]
-
-thanksgiving.evenings <- thanksgiving[hour(hms(thanksgiving$Time)) >= 18 & hour(hms(thanksgiving$Time)) < 24,]
-thanksgiving.nights <- thanksgiving[hour(hms(thanksgiving$Time)) >= 0 & hour(hms(thanksgiving$Time)) < 6,]
-thanksgiving.mornings <- thanksgiving[hour(hms(thanksgiving$Time)) >= 6 & hour(hms(thanksgiving$Time)) < 12,]
-thanksgiving.afternoons <- thanksgiving[hour(hms(thanksgiving$Time)) >= 12 & hour(hms(thanksgiving$Time)) < 18,]
-
-black_friday.evenings <- black_friday[hour(hms(black_friday$Time)) >= 18 & hour(hms(black_friday$Time)) < 24,]
-black_friday.nights <- black_friday[hour(hms(black_friday$Time)) >= 0 & hour(hms(black_friday$Time)) < 6,]
-black_friday.mornings <- black_friday[hour(hms(black_friday$Time)) >= 6 & hour(hms(black_friday$Time)) < 12,]
-black_friday.afternoons <- black_friday[hour(hms(black_friday$Time)) >= 12 & hour(hms(black_friday$Time)) < 18,]
 
 
 # Intersection of time windows
