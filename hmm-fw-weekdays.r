cdf62a2a325442d9bab6311cf1df8b665e7caccc
Anthony Chao <anthonyqchao@gmail.com>
2018-11-19 15:20:42 -0800

    created HMMs for fw weekdays

--- hmm-fw-weekdays.r
+++ hmm-fw-weekdays.r
@@ -2,15 +2,15 @@
 mod.fw.weekdays.days <- depmix(response = Global_active_power ~ 1, 
                                data = fw.weekdays.days, 
                                nstates = 19,
-                               ntimes = rep(540, 378))
+                               ntimes = c(rep(540, 378), 368))
 
 mod.fw.weekdays.evenings <- depmix(response = Global_active_power ~ 1,
                                  data = fw.weekdays.evenings,
                                  nstates = 19,
-                                 ntimes = )
+                                 ntimes = rep(420 , 378))
 
 mod.fw.weekdays.nights <- depmix(response = Global_active_power ~ 1, 
-                                 data = fw.weekday.snights,
+                                 data = fw.weekday.nights,
                                  nstates = 19,
-                                 ntimes = )
+                                 ntimes = rep(480, 379))
 
--- phase1-lm.r
+++ phase1-lm.r
@@ -8,16 +8,16 @@ rownames(corMtx) <- c("Global intensity", "Voltage", "Global reactive power")
 colnames(corMtx) <- c("Global active power")
 corMtx # Highest correlation with Global active power is Global intensity
 
-# Use Global intensity to create our linear regression model
+# Use Global intensity to create our linear regression model with Global active power
 lm.fw <- lm(Global_active_power ~ Global_intensity, data=fw)
 summary(lm.fw)
 
 # Use the linear model lm.fw to predict our test1 dataset values
 lmPred.t1.fw <- predict(lm.fw, t1.fw)
 
+# Prediction accuracy measures
 actuals_preds <- data.frame(cbind(actuals=t1.fw$Global_active_power, predicted=lmPred.t1.fw))
 cor(t1.fw$Global_active_power, lmPred.t1.fw)
-
 # Min-max accuracy - higher the better
 print(mean(apply(actuals_preds, 1, min) / apply(actuals_preds, 1, max))) 
 # Mean absolute percentage error - lower the better
@@ -28,8 +28,6 @@ ggplot() +
   geom_line(data = actuals_predssumm, aes(actuals, pred), colour='red', size=1)  
 
 library(DAAG)
-  
 cvResults <- suppressWarnings(CVlm(fw, form.lm=Global_active_power ~ Global_intensity, m=5, dots=F, seed=1,
                                    legend.pos='topleft', printit=F))
-
 attr(cvResults, 'ms')
